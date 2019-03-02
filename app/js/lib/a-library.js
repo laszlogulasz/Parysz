4408d9092e297497d85f3532e50e76e2a5841d80
Michał Krul <michal@krul.pl>
2019-03-02 21:03:18 +0100

    WIP: working gallery and references

--- app/js/lib/a-library.js
+++ app/js/lib/a-library.js
@@ -1,64 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var mySwiper1 = new Swiper("#first", {
-		// Optional parameters
-
-		loop: true,
-		// If we need pagination
-		pagination: {
-			el: ".swiper-pagination"
-		},
-
-		// Navigation arrows
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev"
-		},
-
-		// And if we need scrollbar
-		scrollbar: {
-			el: ".swiper-scrollbar"
-		}
-	});
-	var swiper = new Swiper("#second", {
-		// Enable lazy loading
-		preloadImages: false,
-		lazy: true,
-		init: false,
-
+		slidesPerView: "auto",
+		spaceBetween: 35,
 		pagination: {
 			el: ".swiper-pagination",
 			clickable: true
-		},
+		}
+	});
+});
+
+const projects = document.querySelectorAll(".project");
+projects.forEach((project, i) => {
+	const projectsSwiper = new Swiper(".second", {
+		init: false,
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev"
 		}
 	});
 
-	MicroModal.init({
-		onShow: modal => swiper.init(), // [1]
-		//onClose: modal => swiper.destroy(), // [2]
-		openTrigger: "data-custom-open", // [3]
-		closeTrigger: "data-custom-close", // [4]
-		disableScroll: true, // [5]
-		disableFocus: false, // [6]
-		awaitCloseAnimation: false, // [7]
-		debugMode: true // [8]
+	project.addEventListener("click", () => {
+		MicroModal.show(`modal-${i + 1}`, {
+			debugMode: true,
+			disableScroll: true,
+			onShow: modal => projectsSwiper[i].init(),
+
+			closeTrigger: "data-custom-close",
+			awaitCloseAnimation: true
+		});
+		setTimeout(function() {
+			projectsSwiper[i].update();
+		}, 10);
 	});
 
-	const projects = document.querySelectorAll(".project");
-	(() => console.log("TCL: projects", projects))();
-
-	projects.forEach((project, i) =>
-		project.addEventListener("click", () => {
-			MicroModal.show(`modal-${i}`, {
-				debugMode: false,
-				disableScroll: true,
-				onShow: modal => swiper.init(),
-				closeTrigger: "data-custom-close",
-				awaitCloseAnimation: true
-			});
-			console.log("clicked");
-		})
-	);
+	console.log("TCL: projectsSwiper", projectsSwiper);
 });
