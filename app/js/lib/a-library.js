204f8f5ab9b84f03e9fa9ae96594de2dffb57715
Michał Krul <michal@krul.pl>
2019-03-03 19:10:50 +0100

    WIP: working carousel

--- app/js/lib/a-library.js
+++ app/js/lib/a-library.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	var mySwiper1 = new Swiper("#first", {
+	const headerSwiper = new Swiper("#headerSwiper", {
+		effect: "fade",
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false
+		}
+	});
+
+	const refSwiper = new Swiper("#first", {
 		slidesPerView: "auto",
 		spaceBetween: 35,
 		pagination: {
@@ -7,31 +15,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			clickable: true
 		}
 	});
-});
 
-const projects = document.querySelectorAll(".project");
-projects.forEach((project, i) => {
-	const projectsSwiper = new Swiper(".second", {
-		init: false,
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev"
-		}
-	});
-
-	project.addEventListener("click", () => {
-		MicroModal.show(`modal-${i + 1}`, {
-			debugMode: true,
-			disableScroll: true,
-			onShow: modal => projectsSwiper[i].init(),
-
-			closeTrigger: "data-custom-close",
-			awaitCloseAnimation: true
+	const projects = document.querySelectorAll(".project");
+	projects.forEach((project, i) => {
+		const projectsSwiper = new Swiper(".second", {
+			init: false,
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev"
+			}
 		});
-		setTimeout(function() {
-			projectsSwiper[i].update();
-		}, 10);
-	});
 
-	console.log("TCL: projectsSwiper", projectsSwiper);
+		project.addEventListener("click", () => {
+			MicroModal.show(`modal-${i + 1}`, {
+				debugMode: true,
+				disableScroll: true,
+				onShow: modal => projectsSwiper[i].init(),
+
+				closeTrigger: "data-custom-close",
+				awaitCloseAnimation: true
+			});
+			setTimeout(function() {
+				projectsSwiper[i].update();
+			}, 10);
+		});
+
+		console.log("TCL: projectsSwiper", projectsSwiper);
+	});
 });
