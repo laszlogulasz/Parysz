3f00ef7ba10e9fe7a1c5f1d15885976e24d58887
Michał Krul <michal@krul.pl>
2019-03-05 15:03:56 +0100

    Working app

--- app/js/lib/library.js
+++ app/js/lib/library.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+const startMenu = () => {
+	const menuItems = document.querySelectorAll(".nav__elem");
+	const menu = document.querySelector(".menu");
+	const hamburger = document.querySelector(".burger");
+	const toggleMenu = () => menu.classList.toggle("visible");
+	const closeMenu = () => menu.classList.remove("visible");
+	menuItems.forEach(item =>
+		item.addEventListener("click", () => {
+			closeMenu();
+			hamburger.classList.remove("is-active");
+		})
+	);
+	hamburger.addEventListener("click", () => {
+		toggleMenu();
+		hamburger.classList.toggle("is-active");
+	});
+};
+
+const startSwipers = () => {
 	const headerSwiper = new Swiper("#headerSwiper", {
 		effect: "fade",
 		autoplay: {
@@ -6,10 +24,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			disableOnInteraction: false
 		}
 	});
+	//const gutter = ;
 
 	const refSwiper = new Swiper("#first", {
 		slidesPerView: "auto",
-		spaceBetween: 35,
+		spaceBetween: window.innerWidth > 768 ? 35 : 18,
 		pagination: {
 			el: ".swiper-pagination",
 			clickable: true
@@ -28,18 +47,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 		project.addEventListener("click", () => {
 			MicroModal.show(`modal-${i + 1}`, {
-				debugMode: true,
+				debugMode: false,
 				disableScroll: true,
 				onShow: modal => projectsSwiper[i].init(),
-
 				closeTrigger: "data-custom-close",
+				disableFocus: true,
 				awaitCloseAnimation: true
 			});
 			setTimeout(function() {
 				projectsSwiper[i].update();
-			}, 10);
+			}, 300);
 		});
-
-		console.log("TCL: projectsSwiper", projectsSwiper);
 	});
+};
+
+document.addEventListener("DOMContentLoaded", e => {
+	startMenu();
+	startSwipers();
 });
